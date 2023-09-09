c93198270a36f8c7d4401f0338bcb1a139324d41
Mustafa Onur Parlak <monurparlak@gmail.com>
2023-09-09 07:44:57 +0300

    pointer initialized

--- _6-pointer/main.cpp
+++ _6-pointer/main.cpp
@@ -1,24 +1,80 @@
-///////////////////////////////////////////////////
-/*-----------------------------------------------
-// CPP Pointer
--------------------------------------------------*/
-///////////////////////////////////////////////////
+/////////////////////////////////////
+/* Ex_1
+#include <cstdio>
 #include <iostream>
 
-constexpr int SIZE = 3;
+int main() {
+  char dummyCharVal = 'a';
+  int dummyIntegerVal = 13;
+  double dummyDoubleVal = 26.123;
+
+  std::cout << "dummyCharVal    : " << dummyCharVal << std::endl;
+  std::cout << "dummyIntegerVal : " << dummyIntegerVal << std::endl;
+  std::cout << "dummyDoubleVal  : " << dummyDoubleVal << std::endl << std::endl;
+
+  std::cout << "Size of dummyCharVal    : " << sizeof(char) << std::endl;
+  std::cout << "Size of dummyIntegerVal : " << sizeof(int) << std::endl;
+  std::cout << "Size of dummyDoubleVal  : " << sizeof(double) << std::endl
+            << std::endl;
+
+  std::cout << "Address of dummyCharVal    : " << &dummyCharVal << std::endl;
+  std::cout << "Address of dummyIntegerVal : " << &dummyIntegerVal << std::endl;
+  std::cout << "Address of dummyDoubleVal  : " << &dummyDoubleVal << std::endl;
+
+  return 0;
+} */
+/////////////////////////////////////
+/* Ex_2
+#include <cstdio>
+#include <iostream>
 
 int main() {
-    int a[SIZE] = {1000, 100, 10};
-    int i = 0; 
+  int hotel = 5; // 4 byte
+  int *hotelptr; // 4 byte
 
-    int *lastPtr = &a[SIZE - 1];
-    int *aptr = a;
+  double oscar = 11.1612; // 8 byte
+  double *oscarptr;       // 8 byte
 
-    while (aptr ++ <= lastPtr) {
-        i++;
-        std::cout << "a[" << i << "] : " << *(a + i - 1) <<std::endl;
-    }
+  hotelptr = &hotel;
+  oscarptr = &oscar;
+
+  std::cout << "hotel: " << hotel << std::endl; // Masked pointer
+  std::cout << "hotelptr value: " << *hotelptr << std::endl;
+
+  std::cout << "oscar: " << oscar << std::endl; // Masked pointer
+  std::cout << "oscarptr value: " << *oscarptr << std::endl;
+
+  return 0;
+} */
+/////////////////////////////////////
+/* Ex_3
+#include <cstdio>
+#include <iostream>
+
+int main() {
+  int hotel = 5; // 4 byte
+  int *hotelptr; // 4 byte
+
+  double oscar = 11.1612; // 8 byte
+  double *oscarptr;       // 8 byte
+
+  hotelptr = &hotel;
+  oscarptr = &oscar;
+
+  std::cout << "hotel: " << hotel << std::endl; // Masked pointer
+  std::cout << "hotelptr value: " << *hotelptr << std::endl;
+
+  std::cout << "oscar: " << oscar << std::endl; // Masked pointer
+  std::cout << "oscarptr value: " << *oscarptr << std::endl;
+
+  return 0;
+} */
+/////////////////////////////////////
+/* Ex_4 */
+#include <cstdio>
+#include <iostream>
+
+int main() {
+  std::cout << "" << std::endl;
+  return 0;
 }
-
-
-
