33da53f6375a17b2818fdee4d783261bc5b08d64
Mustafa Onur Parlak <monurparlak@gmail.com>
2023-10-02 22:23:23 +0300

    class - myclass.hpp added

---
+++ _9-class/myClass.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+
+class myClass {
+private:
+  int counter = 0;
+
+public:
+  static int sCount;
+  static int sValue;
+
+  void func() const;
+  static void sfunc(); // Just declare it here, no 'static' keyword needed
+};
+
+int myClass::sCount = 3;
+
+void myClass::func() const
+{
+  std::cout << "Test" << myClass::sCount << std::endl;
+}
+
+// Define the static member function outside of the class definition
+void myClass::sfunc()
+{
+  std::cout << "sfunc " << std::endl;
+}
